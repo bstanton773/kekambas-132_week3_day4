397af32298b786a551c60d986d98fec529d9a1d3
Brian <bstanton773@gmail.com>
2023-10-05 12:11:39 -0500

    Setup Blog Class, User Class, and create new user for Blog

---
+++ app/blog.py
@@ -0,0 +1,21 @@
+from app.models import User
+
+class Blog:
+    def __init__(self):
+        self.users = []
+        self.posts = []
+
+    # Method to a create a new user instance and add to the Blog's user list
+    def create_new_user(self):
+        # Get user info from input
+        username = input('Please enter a username: ')
+        # Check to see if a user with that username already exists
+        if username in [u.username for u in self.users]:
+            print(f"User with the username {username} already exists.")
+        else:
+            password = input('Please enter a password: ')
+            # Create a new instance of User with the inputted info
+            new_user = User(username, password)
+            # Add the new user to the blog's user list
+            self.users.append(new_user)
+            print(f"{new_user} has been created.")
